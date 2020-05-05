160cd87f2cbed6766bc0b485de56d299b4e03b00
Mathieu-Cabare <mathieu.cabare@gmail.com>
2020-05-05 15:35:14 +0200

    list modif

--- index.php
+++ index.php
@@ -59,6 +59,15 @@
                     </a>
         </div>
 
+        <ul class="list-group col-md-6 mx-auto" >
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+
 </div>
     
 
